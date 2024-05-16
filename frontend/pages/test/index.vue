c5e8e1c43a497521dd9399ebe8c9d25cde482d78
engsiam <siam.softwareengineer.com>
2024-05-16 19:59:33 +0600

    add nuxt layout and component setup for ready to build project structure

--- frontend/pages/test/index.vue
+++ frontend/pages/test/index.vue
@@ -1,5 +1,17 @@
+<script setup>
+definePageMeta({
+  layout: "custome",
+});
+</script>
 <template>
-	<div>
-		hello 8
-	</div>
+  <div class="p-4 text-white bg-pink-500">
+	<nuxt-link to="/">Home</nuxt-link>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, magni.
+    </p>
+    <h2>welcome</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, debitis!
+    </p>
+  </div>
 </template>
