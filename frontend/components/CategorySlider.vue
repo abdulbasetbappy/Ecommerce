4480cf1476dd3cd5bde857249a619d3ccf43a6de
Mustak Ahmed Kafy <ahmedmustak99@gmail.com>
2024-05-18 09:46:25 +0600

    banner add

---
+++ frontend/components/CategorySlider.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="grid grid-cols-12 gap-4">
+    <div class="col-span-4">
+      jskjdnkd
+      <Category />
+    </div>
+    <div class="col-span-8">
+      <Slider />
+      
+    </div>
+  </div>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,8 +3,5 @@
 </script>
 
 <template>
-  <div class="h-screen">
-    <nuxt-link to="/test">test page</nuxt-link>
-    <p class="text-7xl">Home page</p>
-  </div>
+  <CategorySlider />
 </template>
