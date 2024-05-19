006a2c6cff19e65b508a74c8a251f6fd6212582d
Mustak Ahmed Kafy <ahmedmustak99@gmail.com>
2024-05-19 23:34:47 +0600

    banner complete

--- frontend/components/CategorySlider.vue
+++ frontend/components/CategorySlider.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class=" px-4 pt-4 lg:px-20">
-    <div class="grid grid-cols-12 gap-4">
+  <div class="px-4 py-4 lg:px-20">
+    <div class="grid md:grid-cols-12 grid-cols-1  gap-4">
       <div class="col-span-4">
         <Category />
       </div>
