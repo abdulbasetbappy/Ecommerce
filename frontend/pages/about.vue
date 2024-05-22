c5f9470a948dda3219d894f3034e28bbd6fbcc58
Mustak Ahmed Kafy <ahmedmustak99@gmail.com>
2024-05-22 17:48:39 +0600

    slider done

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -54,17 +54,17 @@
         clickable: true,
       }"
       :breakpoints="{
-        640: {
+        '300': {
           slidesPerView: 1,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 2,
           spaceBetween: 20,
         },
-        1024: {
+        '768': {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        '1024': {
           slidesPerView: 3,
-          spaceBetween: 30,
+          spaceBetween: 10,
         },
       }"
       :modules="modules"
